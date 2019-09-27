393779302e747bae6fe2dfa109b42c2010b7b49c
= <=>
2019-09-27 16:09:52 -0300

    arquivos secundários

---
+++ index.h
@@ -0,0 +1,18 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<title>Controle Restaurantes e Bares</title>
+</head>
+<body>
+<h1>Hello Word</h1>
+<h1>Ola Fernando</h1>
+<form action="/get">
+<h2>Nome:</h2>
+<input type='text' name='texto'>
+<input type='submit' value = 'enviar'> 
+</form>
+</body>
+</html>
+)=====";
